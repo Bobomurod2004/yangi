2797f4c575d7dde5340948cecbc7953441461103
Bobomurod2004 <bobomurodgoibnazarov@gmail.com>
2025-01-22 17:18:49 +0500

    faqat bittasi

--- uyga vazifa/LeedCode_masalalari.py
+++ uyga vazifa/LeedCode_masalalari.py
@@ -1,10 +1,52 @@
-def sonlar(x):
+# def sonlar(x):
+#
+#     tekshirish=str(x)
+#     natija_x=tekshirish[::-1]
+#     return tekshirish==natija_x
+#
+# x=int(input('Son kiriting='))
+# a=sonlar(x)
+# print(a)
+# 1 masala
 
-    tekshirish=str(x)
-    natija_x=tekshirish[::-1]
-    return tekshirish==natija_x
+# def sonlar(nums,target):
+#     nums=[2,7,3,6]
+#     target= str (9)
+#     natija=nums[0]+nums[1]
+#     if target==natija:
+#         print(" natija",natija)
+#     else:
+#         print("xato")
+#
+# print(sonlar())
 
-x=int(input('Son kiriting='))
-a=sonlar(x)
-print(a)
+# def kvadrat(son):
+#      return  son**2
+# son=int(input("son kiriting="))
+# natija=kvadrat(son)
+# print("Soning kavadrati=",natija)
+
+# def juftga_tekshirish(son):
+#     if son%2==0:
+#         return f"{son}  Biz kiritgan son juft son hisoblanadi"
+#     else:
+#         return f"{son}  Biz kiritgan son juft son emas"
+#
+# son=int(input("Iltimos son kiriting="))
+# natija=juftga_tekshirish(son)
+# print(natija)
+
+
+# def qoshish(son1, son2):
+#     return son1+son2
+# son1=int(input(f"Iltimos son kiriting="))
+# son2=int(input("Iltimos son kiriting="))
+# natija=qoshish(son1, son2)
+# print("Bizning natija", natija)
+
+# def teskarisi(matn):
+#     return matn [::-1]
+# matn=str(input("Biron bir matin kiriting="))
+# natija=teskarisi(matn)
+# print(f"Biz kiritgan matning {matn} teskarisi", natija)
 
